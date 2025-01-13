bf3dc5ed6cf39d1dc4c96df5bac082f05036e106
pssc <phillip.camp+git@gmail.com>
2025-01-13 22:49:02 +0000

    move staus preperation into library

--- pysqueezebox/const.py
+++ pysqueezebox/const.py
@@ -7,4 +7,16 @@ TIMEOUT = 10.0
 REPEAT_MODE = ["none", "song", "playlist"]
 SHUFFLE_MODE = ["none", "song", "album"]
 
+STATUS_SENSOR_LASTSCAN = "lastscan"
+STATUS_SENSOR_NEEDSRESTART = "needsrestart"
+STATUS_SENSOR_RESCAN = "rescan"
+STATUS_QUERY_LIBRARYNAME = "libraryname"
+STATUS_QUERY_MAC = "mac"
+STATUS_QUERY_UUID = "uuid"
+STATUS_QUERY_VERSION = "version"
+STATUS_UPDATE_NEWVERSION = "newversion"
+STATUS_UPDATE_NEWPLUGINS = "newplugins"
+UPDATE_PLUGINS_RELEASE_SUMMARY = "update_plugins_release_summary"
+UPDATE_RELEASE_SUMMARY = "update_release_summary"
+
 QueryResult: TypeAlias = "dict[str, int | str | QueryResult | list[QueryResult]]"
