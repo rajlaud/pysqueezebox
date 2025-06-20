5fbb6cabe9a24df65303a547f0c4ef61fe9fa81f
Wolfgang Miller-Reichling <wolfgang.miller-reichling@1und1.de>
2025-06-20 08:50:42 +0200

    Revert "also expose alarm state on upcoming alarms"
    Looking again at LMS docs, Alarm State is not binary, but also has "active" and
    "snooze", so this needs a different approach, a binary sensor in HA does
    not work.
    
    This reverts commit 034e45191506f29b493024fe7e9f826e6e73ea69.

--- pysqueezebox/player.py
+++ pysqueezebox/player.py
@@ -510,12 +510,9 @@ class Player:
         return self._player_prefs.get("alarmsEnabled") == "1"
 
     @property
-    def alarm_state(self) -> bool | None:
-        """Return the current alarm state, meaning an upcoming alarm is set or not"""
-        state_str = self._status.get("alarm_state")
-        if state_str is not None:
-            return state_str == "set"
-        return None
+    def alarm_state(self) -> str | None:
+        """Return the current alarm state"""
+        return self._status.get("alarm_state")
 
     @property
     def alarm_next(self) -> datetime | None:
